832ed2c79d2c436c09ce51eaece74e217201d545
Atreya Chandan <atreyachandan786@gmail.com>
2024-01-11 16:17:57 +0530

    Minor bug

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -66,7 +66,7 @@ export default function Checkout() {
               </div>
             </div>
 
-            <div className="sm:col-span-4">
+            <div className="col-span-full">
               <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -123,7 +123,7 @@ export default function Checkout() {
               </div>
             </div>
 
-            <div className="sm:col-span-2 sm:col-start-1">
+            <div className="col-span-full">
               <label
                 htmlFor="city"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -142,7 +142,7 @@ export default function Checkout() {
               </div>
             </div>
 
-            <div className="sm:col-span-2">
+            <div className="col-span-full">
               <label
                 htmlFor="region"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -161,7 +161,7 @@ export default function Checkout() {
               </div>
             </div>
 
-            <div className="sm:col-span-2">
+            <div className="col-span-full">
               <label
                 htmlFor="postal-code"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -181,7 +181,7 @@ export default function Checkout() {
             </div>
             <button
               type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="col-span-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Place your order
             </button>
