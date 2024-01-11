b0d7a3f8c9c27a577ce5fbd0f68a747adab5a8f5
Atreya Chandan <atreyachandan786@gmail.com>
2024-01-11 15:20:55 +0530

    Checkout Completed

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  Cart,
+  FlexContent,
+  Footer,
+  Hero,
+  Navbar,
+  Sales,
+  Stories,
+} from "./components";
+import {
+  heroapi,
+  popularsales,
+  allproducts,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data.js";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Checkout from "./components/Checkout.jsx";
+import { useSelector } from "react-redux";
+import { selectCartItems } from "./app/CartSlice.js";
+
+const App = () => {
+  const cartItems = useSelector(selectCartItems);
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/allProduct"
+          element={
+            <>
+              <Navbar />
+              <Cart />
+              <main className="flex flex-col gap-16 relative">
+                <Sales endpoint={allproducts} ifExists />
+              </main>
+              <Footer footerAPI={footerAPI} />
+            </>
+          }
+        />
+        <Route
+          path="/checkout"
+          element={
+            cartItems.length > 0 && (
+              <>
+                <Navbar />
+                <main className="flex flex-col gap-16 relative">
+                  <Checkout />
+                </main>
+                <Footer footerAPI={footerAPI} />
+              </>
+            )
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Cart />
+              <main className="flex flex-col gap-16 relative">
+                <Hero heroapi={heroapi} />
+                <FlexContent endpoint={highlight} ifExists />
+                <Sales endpoint={popularsales} ifExists />
+                <FlexContent endpoint={sneaker} />
+                <Stories story={story} />
+              </main>
+              <Footer footerAPI={footerAPI} />
+            </>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Checkout.jsx
@@ -0,0 +1,193 @@
+import { useNavigate } from "react-router-dom";
+import { setClearCartItemsAfterCheckout, setCloseCart } from "../app/CartSlice";
+import { useDispatch } from "react-redux";
+
+export default function Checkout() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  return (
+    <form
+      className="flex justify-center items-center mt-20 sm:mt-16"
+      onSubmit={() => {
+        dispatch(setClearCartItemsAfterCheckout());
+        dispatch(
+          setCloseCart({
+            cartState: false,
+          })
+        );
+        navigate("/");
+      }}
+    >
+      <div className="space-y-12 w-[40%] sm:w-[70%]">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Personal Information
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            Use a permanent address where you can receive orders.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                First name
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Last name
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Country
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="street-address"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Street address
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  name="street-address"
+                  id="street-address"
+                  autoComplete="street-address"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label
+                htmlFor="city"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                City
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  name="city"
+                  id="city"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="region"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                State / Province
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  name="region"
+                  id="region"
+                  autoComplete="address-level1"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="postal-code"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                ZIP / Postal code
+              </label>
+              <div className="mt-1">
+                <input
+                  required
+                  type="text"
+                  name="postal-code"
+                  id="postal-code"
+                  autoComplete="postal-code"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Place your order
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
